6ff9a19fe9c25b8e5028a3276579288ec4242b57
Test User <y2yogesh13@gmail.com>
2020-05-21 15:18:50 +0530

    Changed Files

--- Hello.java
+++ Hello.java
@@ -2,10 +2,6 @@ public class Hello{
 	
 	public static void main (String args[]){
 	
-		for(int i =0; i<10 ; i++){
-			
-			System.out.println("Hello World...." +i);
-		}
-	
+			System.out.println("Hello World");
 	}
 }
